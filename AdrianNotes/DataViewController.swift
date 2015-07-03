44f86c941b8f14ddb937a435c66a49fb91653d94
AdrianHsu <adrianhsu1995@gmail.com>
2015-07-03 23:15:04 +0800

    first commit

---
+++ AdrianNotes/DataViewController.swift
@@ -0,0 +1,38 @@
+//
+//  DataViewController.swift
+//  AdrianNotes
+//
+//  Created by AdrianHsu on 2015/7/3.
+//  Copyright (c) 2015年 AdrianHsu. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: AnyObject?
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        if let obj: AnyObject = dataObject {
+            self.dataLabel!.text = obj.description
+        } else {
+            self.dataLabel!.text = ""
+        }
+    }
+
+
+}
+
